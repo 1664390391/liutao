e50b67a7fd0d76c60faeb5ee44bb0dd117660ad7
LIU\Administrator <1664390391@qq.com>
2018-08-30 09:07:40 +0800

    no message

--- myweb/src/javase/stringstudy/StringBase.java
+++ myweb/src/javase/stringstudy/StringBase.java
@@ -17,9 +17,22 @@ public class StringBase {
 //        String s1 = s.intern();
 //        System.out.println(s1);
 
-        System.out.println(Boolean.parseBoolean("true"));
+//        System.out.println(Boolean.parseBoolean("true"));
+//
+//        Integer i = new Integer("111");
+//        System.out.println(i.intValue());
 
 //        int c = (int)'a';
 //        System.out.println((char)(c-32));
+
+        Integer i1 = 127;
+        Integer i2 = 127;
+        System.out.println(i1 == i2); // true
+        System.out.println(i1.equals(i2)); // true
+        Integer i3 = 128;
+        Integer i4 = 128;
+        System.out.println(i3 == i4);  // false
+        System.out.println(i3.equals(i4)); // true
+
     }
 }
