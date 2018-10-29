78a158446a6f99ac791361b431c8a19bff9f1595
liutao <liutao.1997@qq.com>
2018-10-29 23:55:23 +0800

    no message

--- myweb/src/javase/io/othercommonclass/bean/Person.java
+++ myweb/src/javase/io/othercommonclass/bean/Person.java
@@ -8,6 +8,16 @@ import java.io.Serializable;
 public class Person implements Serializable{
     private String name;
     private int age;
+    /*
+    不被序列化的字段
+    字段一旦是静态的，那么随着类的加载而加载，并有初始化的默认值，
+    这个时候对象进行操作的只是对象特有的一些属性字段 ，序列化对象也不会序列化这些字段。
+    private static int age;
+    非静态字段，又不想被序列化，使用该关键字进行修饰
+    private transient int age;
+
+     */
+
     //显示声明自定义的serialVersionUID
     static final long serialVersionUID = 12312421412L;
 
